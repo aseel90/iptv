e0411f51bea199a80b02dc496eb6a17f91b5caf6
aseel salah <aseel.salah90@yahoo.com>
2026-09-10 18:04:54 +0200

    Instrument startup stages for Android TV crash isolation

--- app/src/main/java/com/selyro/tv/MainActivity.kt
+++ app/src/main/java/com/selyro/tv/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.selyro.tv
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.SideEffect
 import androidx.core.view.WindowCompat
 import com.selyro.tv.ui.SelyroApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("SelyroStartup", "activity:onCreate:start")
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        setContent { SelyroApp() }
+        Log.i("SelyroStartup", "activity:setContent:before")
+        setContent {
+            SideEffect { Log.i("SelyroStartup", "compose:first-composition:ok") }
+            SelyroApp()
+        }
+        Log.i("SelyroStartup", "activity:setContent:after")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("SelyroStartup", "activity:onResume")
     }
 }
